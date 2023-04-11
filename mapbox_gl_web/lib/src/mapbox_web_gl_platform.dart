a464efaa964f50e995d2859c1f82dc937e6b1e1f
Rim El Jammal <rim.eljammal@ocell.io>
2023-04-11 15:33:26 +0200

    revert duration changes in animateCamera

--- mapbox_gl_web/lib/src/mapbox_web_gl_platform.dart
+++ mapbox_gl_web/lib/src/mapbox_web_gl_platform.dart
@@ -180,25 +180,13 @@ class MapboxWebGlPlatform extends MapboxGlPlatform
   }
 
   @override
-  Future<bool?> animateCamera(CameraUpdate cameraUpdate,
-      {Duration? duration}) async {
+  Future<bool?> animateCamera(
+    CameraUpdate cameraUpdate, {
+    Duration? duration,
+  }) async {
     final cameraOptions = Convert.toCameraOptions(cameraUpdate, _map);
 
-    final around = getProperty(cameraOptions, 'around');
-    final bearing = getProperty(cameraOptions, 'bearing');
-    final center = getProperty(cameraOptions, 'center');
-    final pitch = getProperty(cameraOptions, 'pitch');
-    final zoom = getProperty(cameraOptions, 'zoom');
-
-    _map.flyTo({
-      if (around.jsObject != null) 'around': around,
-      if (bearing != null) 'bearing': bearing,
-      if (center.jsObject != null) 'center': center,
-      if (pitch != null) 'pitch': pitch,
-      if (zoom != null) 'zoom': zoom,
-      if (duration != null) 'duration': duration.inMilliseconds,
-    });
-
+    _map.flyTo(cameraOptions);
     return true;
   }
 
@@ -1045,7 +1033,7 @@ class MapboxWebGlPlatform extends MapboxGlPlatform
   @override
   Future<bool?> getLayerVisibility(String layerId) async {
     final result = _map.getLayoutProperty(layerId, 'visibility');
-    if(result == null) return true;
+    if (result == null) return true;
     if (result is String) return result == "visible";
     return false;
   }
